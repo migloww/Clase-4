844e4fe9b16422ba5b97f72b158ed43bb673a425
migloww <miguelruizalzate04@gmail.com>
2024-08-15 07:17:05 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,9 +1,9 @@
 import streamlit as st
-
+from PIL import image
 
 st.title("PEDRO SE QUEDO DORMIDO")
 st.header("pedro tenia que venir a clase pero probablemente se quedo dormido producto del cansancio que supone el inicio de semestre")
-from PIL import image
+
 image = Image.open("images.jpg")
 st.image(image, caption="lil yachty")
 
