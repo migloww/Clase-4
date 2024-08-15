df7b84784008eb0335188ef3bbc46dea01daf541
migloww <miguelruizalzate04@gmail.com>
2024-08-15 07:29:55 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -9,3 +9,18 @@ st.image(image, caption="lil yachty")
 
 texto = st.text_input("hecho", "Podemos decir que hay muchas similitudes entre pedro y lil yatchy")
 st.write("podriamos enumerarlas pero nunca terminariamos",texto)
+
+st.subheader("ahora usaremos 2 columnas")
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("esta es la primera columna")
+  st.write("las interfaces multimodales mejoran la experiencia de usuario")
+  resp = st.chekbox("estoy de acuerdo")
+  if resp:
+    st.write("correcto!")
+
+
+
+
